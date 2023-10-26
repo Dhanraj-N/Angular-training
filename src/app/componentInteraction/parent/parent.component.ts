1e912089680c2b1b39aa4656e35c3cf25f0a855e
Dnimalwar <dnimalwar@gmail.com>
2023-10-26 15:34:50 +0530

    Call function in get data from input

--- src/app/componentInteraction/parent/parent.component.ts
+++ src/app/componentInteraction/parent/parent.component.ts
@@ -1,21 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit {
+ 
   strData='This message is from parent';
   dataFromChild = 0;
+  parentClickNo = 0;
 
-  onChildClick(data:any){
-     this.dataFromChild=data
+  ngOnInit(): void {
+    this.fun()
+  }
+  onChildClick(data:any): void {
+    this.dataFromChild = data;
   }
 
-  onParentClick(){
-  let parentClickNo=Math.random()+10
-  console.log(parentClickNo);
+   onParentClick(): void {
+    this.parentClickNo = Math.random();
   }
   
+  fun(){
+    this.observable.subscribe(
+      {
+        next(x) {
+          console.log('got value ' + x);
+        }
+      }
+    )
+  }
+ observable=new Observable ((sub)=>{
+  sub.next(11);
+  sub.next(21);
+  sub.next(31);
+
+ setTimeout(()=>{
+   sub.next(400);
+   console.log("h");
+   }, 1000)
+  sub.complete()
+ })
+
+ 
+ 
+
+
 }
