dcaec4520cf7c554021a87c6eda5e081ee0f03b5
Dnimalwar <dnimalwar@gmail.com>
2023-11-03 19:03:37 +0530

    API Integration- PUT and DELETE

--- src/app/api-integration/api-integration.component.ts
+++ src/app/api-integration/api-integration.component.ts
@@ -1,63 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-api-integration',
-  templateUrl: './api-integration.component.html',
-  styleUrls: ['./api-integration.component.css']
+ selector: 'app-api-integration',
+ templateUrl: './api-integration.component.html',
+ styleUrls: ['./api-integration.component.css'],
 })
-export class ApiIntegrationComponent implements OnInit{
-  myId = '';
-  myUserId = '';
-  myTitle = '';
-  myBody = '';
-  isHidden = true;
+export class ApiIntegrationComponent implements OnInit {
+ myId = '';
+ myUserId = '';
+ myTitle = '';
+ myBody = '';
+ isHidden = true;
+ submitted = false;
 
-  ApiData: any = [];
-  
-  ApiDataById: any = {}
-  constructor(private service:ApiService) {}
+ apiData: Array<any> = [];
+ formData : Array<any> = [];
 
-  ngOnInit(): void {
-    
+ postId: number | undefined;
+ apiForm !: FormGroup;
+ updateForm !: FormGroup;
+ constructor(private service: ApiService, private fb : FormBuilder) {}
+
+ ngOnInit(): void {
+  this.apiForm = this.fb.group({
+    id : [0], 
+    userId : ['', [Validators.required]], 
+    title : ['', [Validators.required]],
+    body : ['', [Validators.required]],
+  });
+ }
+
+ // Method for GET
+
+ getAllData(): void {
+  this.service.getAllData().subscribe((res) => {
+   this.apiData = res;
+  });
+ }
+
+ getDataById(): void {
+  this.service.getDataById(this.postId).subscribe((res) => {
+   this.apiData = [res];  //{ sdfsdf: sdfsdf}
+  });
+ }
+
+ // Method for POST
+
+  postData(formData : any): void {
+    this.submitted = true;
+    if (this.apiForm.valid) {
+      this.service.postData(formData).subscribe((res) => {
+        this.apiData.push(res);
+     })
+    }
   }
 
-  getData () {
-    this.service.getData().subscribe((res) => {
-    this.ApiData = res;
-    })
-  }
+ // Method for PUT
 
-  getDataById (id: any) {
-    this.service.getDataById(id).subscribe((res) => {
-    console.log(res);
-    this.ApiDataById = res;
-    })
-  }
-  
-  onSubmit(Obj:any) : void {
-    this.service.addData(Obj).subscribe((res) => {
-      console.log(res);
-    })
-  }
+ onEdit(id: any): void {
+  const record = this.apiData.find((res) => {
+  return res.id == id.id
+  })
+  this.apiForm.patchValue(record);
+ }
 
-  deleteData(id:any){
-    this.service.deleteData(id).subscribe((res) => {
-      console.log(res);
-    })
-  }
+ putData(data: any): void {
+  this.service.putData(data).subscribe((res) => {
+   console.log('Record is updated succesfully of Id ' + data.id, res);
+  });
+ }
 
-  onEdit(data: any) : void {
-     this.myId = data.id;
-     this.myUserId = data.userId;
-     this.myTitle = data.title;
-     this.myBody = data.body;
-  }
+ // Method for DELETE
 
-  updateData(data: any) {
-    this.service.updateData(data).subscribe((res) => {
-      console.log(res);
-    })
-  }
+ deleteData(id: any): void {
+  this.service.deleteData(id).subscribe((res) => {
+   console.log('Record is deleted of Id ' + id, res);
+  });
+ }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,37 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../Environment/environment';
 @Injectable({
-  providedIn: 'root'
+ providedIn: 'root',
 })
 export class ApiService {
+ constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) { }
+ // GET
+ public getAllData() {
+  const path: string = '/posts/';
+  return this.http.get<any>(`${environment.API}${path}`);
+ }
 
-  public getData ()  {
-    const path: string = '/posts/';
-    return this.http.get(`${environment.API}${path}`);
-  }
+ public getDataById(id: any) {
+  const path: string = '/posts/';
+  return this.http.get<any>(`${environment.API}${path}${id}`);
+ }
 
-  public getDataById (id:any) {
-    const path: string = '/posts/';
-    return this.http.get(`${environment.API}${path}${id}`);
-  }
+ // POST
 
-  public addData (obj :any) {
-     const path: string = '/posts/';
-     return this.http.post(`${environment.API}${path}`, obj);
-  }
+ public postData(formData: any) {
+  const path: string = '/posts';
+  return this.http.post<any>(`${environment.API}${path}`, formData);
+ }
 
-  
-  public deleteData(id :any) {
-    const path: string = '/posts/';
-    return this.http.delete(`${environment.API}${path}${id}`);
-  }
+ // UPDATE
 
-  public updateData(data: any) {
-    console.log(data);
-    const path: string = '/posts/';
-    return this.http.put(`${environment.API}${path}${data.id}`, data);
-  }
+ public putData(data: any) {
+  const path: string = '/posts/'; 
+  return this.http.put(`${environment.API}${path}${data.id}`, data);
+ }
+
+ // DELETE
+
+ public deleteData(id: any) {
+  const path: string = '/posts/';
+  return this.http.delete(`${environment.API}${path}${id}`);
+ }
+
+
 }
